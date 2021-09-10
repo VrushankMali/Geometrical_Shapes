053fb6aa41e58f67e6b38060153da3c2fa479339
VrushankMali <VrushankMali@github.com>
2021-09-10 01:10:30 -0500

    Add project files.

---
+++ Shapes/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Line
+    {
+        void draw()
+        {
+            System.Console.WriteLine("Line is drawn");
+        }
+    }
+}
---
+++ Shapes/ShapeFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    enum ShapeType
+    {
+        Line,
+        Circle,
+        Rectangle,
+        Triangle
+    }
+    class ShapeFactory
+    {
+        GeometricShape getShape(ShapeType type)
+        {
+
+        }
+    }
+}
